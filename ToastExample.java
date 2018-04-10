ad655b1378069991276c8636ea259bdc9fd24eb8
hatsumi2301 <hatsumih@hotmail.com>
2018-04-10 21:58:13 +0100

    More commits

---
+++ ToastExample.java
@@ -0,0 +1,26 @@
+public class ToastExample extends 
+AppCompatActivity{
+    private Button btnButton;
+
+    @Override
+    public void onCreate(Bundle 
+savedInstanceState){
+        super.onCreate(savedInstanceState);
+        
+setContentView(R.layout.main_activity);
+
+        btnButton = (Button) 
+findViewById(R.id.btnButtonId);
+
+        btnButton.setOnClickListener(new 
+OnClickListener(){
+            @Override
+            public void onClick(View v){
+                
+Toast.makeText(getApplicationContext(), 
+"Mensagem ao clicar no botão", 
+Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
